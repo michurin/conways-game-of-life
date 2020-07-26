a96f06c743b66a0f3a22c4ba38a8ee35d7201945
Alexey Michurin <a.michurin@gmail.com>
2020-07-26 09:11:09 +0300

    Minor rewriting: split logic and view

--- main.js
+++ main.js
@@ -1,194 +1,130 @@
-// You can consider this global variables like state of one global singleton.
-
-// setup
-let gridSizeX;
-let gridSizeY;
-let step;
-let size;
-let context2d;
-
-// state
-let gridValues; // speed up: we won't relocate this memory
-let gridCounters;
-let gridNeighbors;
-let candidates; // speed up: we will focused only on certain places
-
-// constants
-const fillDead = '#333';
-const fillLife = '#8f8';
-
-function reset() {
-  let x;
-  let y;
-  gridValues.fill(0);
-  gridCounters.fill(0);
-  candidates = {};
-  const w = context2d.canvas.clientWidth;
-  const h = context2d.canvas.clientHeight;
-  context2d.fillStyle = fillDead;
-  context2d.fillRect(0, 0, w, h);
-  context2d.fillStyle = '#000';
-  if (step >= 10) {
-    for (x = 0; x <= w; x += step) {
-      context2d.fillRect(x, 0, 1, h);
-    }
-    for (y = 0; y <= h; y += step) {
-      context2d.fillRect(0, y, w, 1);
-    }
-  } else {
-    context2d.fillRect(0, 0, 1, h);
-    context2d.fillRect(w - 1, 0, 1, h);
-    context2d.fillRect(0, 0, w, 1);
-    context2d.fillRect(0, h - 1, w, 1);
+class LGame {
+  constructor() {
+    // public
+    this.width = undefined;
+    this.height = undefined;
+    // private
+    this.values = undefined;
+    this.counters = undefined;
+    this.neighbors = undefined;
+    this.candidates = undefined;
   }
-}
 
-function initGrid() { // have to be private?
-  let x;
-  let y;
-  let a;
-  let b;
-  let a0;
-  let b0;
-  let a1;
-  let b1;
-  let na;
-  // setup grid
-  const gridSize = gridSizeX * gridSizeY;
-  gridValues = Array(gridSize);
-  gridCounters = Array(gridSize);
-  gridNeighbors = Array(gridSize);
-  const sx = gridSizeX - 1;
-  const sy = gridSizeY - 1;
-  for (y = 0; y < gridSizeY; y++) {
-    for (x = 0; x < gridSizeX; x++) {
-      a0 = x === 0 ? 0 : x - 1;
-      a1 = x === sx ? x : x + 1;
-      b0 = y === 0 ? 0 : y - 1;
-      b1 = y === sy ? y : y + 1;
-      na = [];
-      for (b = b0; b <= b1; b++) {
-        for (a = a0; a <= a1; a++) {
-          if (!(a === x && b === y)) {
-            na.push(a + gridSizeX * b);
+  reset() {
+    this.values.fill(0);
+    this.counters.fill(0);
+    this.candidates = {};
+  }
+
+  init() {
+    let x;
+    let y;
+    let a;
+    let b;
+    let a0;
+    let b0;
+    let a1;
+    let b1;
+    let na;
+    // setup grid
+    const s = this.width * this.height;
+    this.values = Array(s);
+    this.counters = Array(s);
+    this.neighbors = Array(s);
+    const sx = this.width - 1;
+    const sy = this.height - 1;
+    for (y = 0; y < this.height; ++y) {
+      for (x = 0; x < this.width; ++x) {
+        a0 = x === 0 ? 0 : x - 1;
+        a1 = x === sx ? x : x + 1;
+        b0 = y === 0 ? 0 : y - 1;
+        b1 = y === sy ? y : y + 1;
+        na = [];
+        for (b = b0; b <= b1; ++b) {
+          for (a = a0; a <= a1; ++a) {
+            if (!(a === x && b === y)) {
+              na.push(a + this.width * b);
+            }
           }
         }
+        this.neighbors[x + this.width * y] = na;
       }
-      gridNeighbors[x + gridSizeX * y] = na;
+    }
+    this.reset();
+  }
+
+  updateElement(i, v, dn) { // unsafe helper for private use only
+    let j;
+    let x;
+    this.values[i] = v;
+    this.candidates[i] = undefined;
+    const nn = this.neighbors[i];
+    for (j = 0; j < nn.length; ++j) {
+      x = nn[j];
+      this.counters[x] += dn;
+      this.candidates[x] = undefined;
     }
   }
-  reset();
-}
 
-function setupGrid(w, h, s) {
-  step = s;
-  gridSizeX = w;
-  gridSizeY = h;
-  let cw = gridSizeX * step + 1;
-  let ch = gridSizeY * step + 1;
-  if (step >= 3) {
-    size = s - 1;
-  } else {
-    size = s;
-    cw += 1;
-    ch += 1;
+  inverse(x, y) {
+    const t = x + this.width * y;
+    const v = this.values[t] ? 0 : 1;
+    this.updateElement(t, v, v === 0 ? -1 : 1);
+    return v;
   }
-  const e = document.getElementById('grid');
-  e.width = cw;
-  e.height = ch;
-  context2d = e.getContext('2d');
-  initGrid();
-}
 
-function updateGridElement(i, v, dn) { // unsafe helper for private use only
-  let j;
-  let x;
-  gridValues[i] = v;
-  candidates[i] = undefined;
-  const nn = gridNeighbors[i];
-  for (j = 0; j < nn.length; j++) {
-    x = nn[j];
-    gridCounters[x] += dn;
-    candidates[x] = undefined;
-  }
-}
-
-function manualSetOnGrid(x, y, vOpt) {
-  if (x < 0 || x >= gridSizeX || y < 0 || y >= gridSizeY) {
-    return;
-  }
-  const t = x + gridSizeX * y;
-  const invert = gridValues[t] ? 0 : 1;
-  const v = vOpt === undefined ? invert : vOpt;
-  if (v === gridValues[t]) {
-    return;
-  }
-  updateGridElement(t, v, v === 0 ? -1 : 1);
-  context2d.fillStyle = [fillDead, fillLife][v];
-  context2d.fillRect(x * step + 1, y * step + 1, size, size);
-}
-
-function stepGrid() {
-  let i;
-  let k;
-  let v;
-  let n;
-  let t;
-  // step 1: analyze
-  const letOff = [];
-  const letOn = [];
-  const keys = Object.keys(candidates); // simple for faster then forEach(f)
-  for (i = 0; i < keys.length; i++) {
-    k = keys[i];
-    v = gridValues[k];
-    n = gridCounters[k];
-    if (v) {
-      if (!(n === 2 || n === 3)) {
-        letOff.push(k);
+  step() {
+    let i;
+    let k;
+    let v;
+    let n;
+    // step 1: analyze
+    const letOff = [];
+    const letOn = [];
+    const keys = Object.keys(this.candidates); // simple for faster then forEach(f)
+    for (i = 0; i < keys.length; ++i) {
+      k = keys[i];
+      v = this.values[k];
+      n = this.counters[k];
+      if (v) {
+        if (!(n === 2 || n === 3)) {
+          letOff.push(k);
+        }
+      } else if (n === 3) {
+        letOn.push(k);
       }
-    } else if (n === 3) {
-      letOn.push(k);
     }
+    // step 2: update candidates, grid, canvas
+    this.candidates = {};
+    for (i = 0; i < letOff.length; ++i) {
+      this.updateElement(letOff[i], 0, -1);
+    }
+    for (i = 0; i < letOn.length; ++i) {
+      this.updateElement(letOn[i], 1, 1);
+    }
+    return [this.width, letOff, letOn];
   }
-  // step 2: update candidates, grid, canvas
-  candidates = {};
-  context2d.fillStyle = fillDead;
-  for (i = 0; i < letOff.length; i++) {
-    t = letOff[i];
-    updateGridElement(t, 0, -1);
-    context2d.fillRect((t % gridSizeX) * step + 1, Math.floor(t / gridSizeX) * step + 1, size, size);
-  }
-  context2d.fillStyle = fillLife;
-  for (i = 0; i < letOn.length; i++) {
-    t = letOn[i];
-    updateGridElement(t, 1, 1);
-    context2d.fillRect((t % gridSizeX) * step + 1, Math.floor(t / gridSizeX) * step + 1, size, size);
-  }
-  return letOff.length > 0 || letOn.length > 0;
-}
-
-function fillGrid(f) {
-  reset();
-  return f(gridSizeX, gridSizeY, (x, y) => manualSetOnGrid(x, y, 1));
 }
 
 // ----------------------------------------------------------------------
 
 function fillSimpleMap(m) {
-  return (w, h, s) => {
+  return (w, h) => {
     let x;
     let y;
+    const s = [];
     const mw = m[0].length;
     const mh = m.length;
     const xo = Math.floor((w - mw) / 2);
     const yo = Math.floor((h - mh) / 2);
-    for (y = 0; y < mh; y++) {
-      for (x = 0; x < mw; x++) {
+    for (y = 0; y < mh; ++y) {
+      for (x = 0; x < mw; ++x) {
         if (m[y][x]) {
-          s(x + xo, y + yo, 1);
+          s.push({ x: x + xo, y: y + yo });
         }
       }
     }
+    return s;
   };
 }
 
@@ -209,9 +145,9 @@ function buildDigitMap(num) { // http://www.radicaleye.com/DRH/digits.html
   let j;
   let c;
   const m = [[], [], [], [], []];
-  for (i = 0; i < num.length; i++) {
+  for (i = 0; i < num.length; ++i) {
     c = p[num.charAt(i)];
-    for (j = 0; j < 5; j++) {
+    for (j = 0; j < 5; ++j) {
       m[j].push.apply(m[j], c[j]);
       if (i + 1 < num.length) {
         m[j].push(0);
@@ -222,21 +158,39 @@ function buildDigitMap(num) { // http://www.radicaleye.com/DRH/digits.html
 }
 
 const fillFunctions = [
-  ['random', function random(w, h, s) {
+  ['random', (w, h) => {
+    const s = [];
     const di = Math.floor(w / 4);
     const dj = Math.floor(h / 4);
-    for (let j = 0; j < h / 2; j++) {
-      for (let i = 0; i < w / 2; i++) {
+    for (let j = 0; j < h / 2; ++j) {
+      for (let i = 0; i < w / 2; ++i) {
         if (Math.random() < 0.4) {
-          s(i + di, j + dj);
+          s.push({ x: i + di, y: j + dj });
         }
       }
     }
+    return s;
+  }],
+  ['benchmark', (w, h) => {
+    const s = [];
+    for (let j = 1; j < h - 1; j += 4) {
+      for (let i = 1; i < w - 1; i += 4) {
+        s.push({ x: i, y: j - 1 });
+        s.push({ x: i, y: j });
+        s.push({ x: i, y: j + 1 });
+      }
+    }
+    return s;
   }],
   ['glider', fillSimpleMap([
     [0, 1, 0],
     [0, 0, 1],
     [1, 1, 1],
+  ])],
+  ['F', fillSimpleMap([
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 1, 0],
   ])],
   ['41140732', fillSimpleMap(buildDigitMap('41140732'))],
   ['90', fillSimpleMap(buildDigitMap('90'))],
@@ -246,7 +200,8 @@ const fillFunctions = [
   ['1415073111111103975114', fillSimpleMap(buildDigitMap('1415073111111103975114'))],
 ];
 
-function lifeMap(w, h, s) {
+function lifeMap(w, h) {
+  const s = [];
   const m = [ // 5x17
     [1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1],
     [1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0],
@@ -267,116 +222,238 @@ function lifeMap(w, h, s) {
   let j;
   let z;
   let p;
-  for (y = 0; y < 5; y++) {
-    for (x = 0; x < 17; x++) {
+  for (y = 0; y < 5; ++y) {
+    for (x = 0; x < 17; ++x) {
       if (m[y][x]) {
-        for (j = 0; j < cs; j++) {
+        for (j = 0; j < cs; ++j) {
           z = y * cs + j;
-          p = ((z / cs / 5) * 2 - 1) ** 2 * 0.4 + 0.2;
-          for (i = 0; i < cs; i++) {
+          p = Math.pow(((z / cs / 5) * 2 - 1), 2) * 0.4 + 0.2;
+          for (i = 0; i < cs; ++i) {
             if (Math.random() < p) {
-              s(cxo + x * cs + i, cyo + y * cs + j, 1);
+              s.push({ x: cxo + x * cs + i, y: cyo + y * cs + j });
             }
           }
         }
       }
     }
   }
+  return s;
 }
 
 // ----------------------------------------------------------------------
 
+class Canvas {
+  constructor(game) {
+    // public
+    this.width = 100;
+    this.height = 100;
+    this.cellStep = 2;
+    // private
+    this.cellSize = 2;
+    this.bdColor = '#000';
+    this.bgColor = '#222';
+    this.fgColor = '#6f6';
+    this.canvas = $('#grid')[0];
+    this.context = this.canvas.getContext('2d');
+    this.prev = '';
+    this.game = game;
+  }
+
+  reset() {
+    this.game.reset();
+    const pw = this.canvas.width;
+    const ph = this.canvas.height;
+    const w = this.game.width;
+    const h = this.game.height;
+    this.context.fillStyle = this.bgColor;
+    this.context.fillRect(0, 0, pw, ph);
+    this.context.fillStyle = this.bdColor;
+    this.context.fillRect(0, 0, pw, 1);
+    this.context.fillRect(0, ph - 1, pw, 1);
+    this.context.fillRect(0, 0, 1, ph);
+    this.context.fillRect(pw - 1, 0, 1, ph);
+    if (this.cellStep > 7) {
+      for (let i = 1; i < w; ++i) {
+        this.context.fillRect(i * this.cellStep, 0, 1, ph);
+      }
+      for (let j = 1; j < h; ++j) {
+        this.context.fillRect(0, j * this.cellStep, pw, 1);
+      }
+    }
+  }
+
+  update() {
+    let extra = 2;
+    this.cellSize = this.cellStep;
+    if (this.cellStep > 2) {
+      extra = 1;
+      this.cellSize = this.cellStep - 1;
+    }
+    const w = Math.floor((this.width - extra) / this.cellStep);
+    const h = Math.floor((this.height - extra) / this.cellStep);
+    //
+    this.game.width = w;
+    this.game.height = h;
+    this.game.init();
+    //
+    const pw = w * this.cellStep + extra;
+    const ph = h * this.cellStep + extra;
+    this.canvas.width = pw;
+    this.canvas.height = ph;
+    this.context = this.canvas.getContext('2d');
+    this.reset();
+  }
+
+  touch(x, y) {
+    const a = Math.floor(x / this.cellStep);
+    const b = Math.floor(y / this.cellStep);
+    const hash = a + ':' + b;
+    if (hash === this.prev) {
+      return;
+    }
+    this.prev = hash;
+    const v = this.game.inverse(a, b);
+    this.context.fillStyle = [this.bgColor, this.fgColor][v];
+    this.context.fillRect(a * this.cellStep + 1, b * this.cellStep + 1, this.cellSize, this.cellSize);
+  }
+
+  touchReset() {
+    this.prev = '';
+  }
+
+  step() {
+    let i;
+    const t1 = new Date();
+    const [w, off, on] = this.game.step();
+    const t2 = new Date();
+    this.context.fillStyle = this.bgColor;
+    for (i = 0; i < off.length; ++i) {
+      const t = off[i];
+      this.context.fillRect((t % w) * this.cellStep + 1, Math.floor(t / w) * this.cellStep + 1, this.cellSize, this.cellSize);
+    }
+    this.context.fillStyle = this.fgColor;
+    for (i = 0; i < on.length; ++i) {
+      const t = on[i];
+      this.context.fillRect((t % w) * this.cellStep + 1, Math.floor(t / w) * this.cellStep + 1, this.cellSize, this.cellSize);
+    }
+    const t3 = new Date();
+    return {
+      updated: off.length + on.length,
+      calcTime: t2 - t1,
+      drawTime: t3 - t2,
+    };
+  }
+
+  fill(f) {
+    this.reset();
+    this.context.fillStyle = this.fgColor;
+    f(this.game.width, this.game.height).forEach((v) => {
+      this.game.inverse(v.x, v.y);
+      this.context.fillRect(v.x * this.cellStep + 1, v.y * this.cellStep + 1, this.cellSize, this.cellSize);
+    });
+  }
+}
+
+class Runner {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.timerId = undefined;
+    // public
+    this.statFrames = 0;
+    this.statUpdated = 0;
+    this.statCalcTime = 0;
+    this.statDrawTime = 0;
+  }
+
+  step() {
+    const res = this.canvas.step();
+    this.statFrames += 1;
+    this.statUpdated += res.updated;
+    this.statCalcTime += res.calcTime;
+    this.statDrawTime += res.drawTime;
+    // TODO account stat
+    if (res.updated > 0) {
+      this.timerId = requestAnimationFrame(() => { this.step(); });
+    }
+  }
+
+  run() {
+    this.stop();
+    this.step();
+  }
+
+  stop() {
+    cancelAnimationFrame(this.timerId);
+  }
+}
+
 $(() => {
-  // TODO make an object for ctl?
-  let timerId;
-  let pixelWidth;
-  let pixelHeight;
-  let pixelStep;
-  let hold = false;
-  function go() {
-    let c = true;
-    if (!hold) {
-      c = stepGrid();
-    }
-    if (c) {
-      timerId = setTimeout(go, 0);
-    } else {
-      timerId = undefined;
-    }
-  }
-  function stop() {
-    if (timerId !== undefined) {
-      clearTimeout(timerId);
-      timerId = undefined;
-    }
-  }
-  function updateGeometry() {
-    stop();
-    setupGrid(Math.floor((pixelWidth - 2) / pixelStep) - 1, Math.floor((pixelHeight - 2) / pixelStep) - 1, pixelStep);
-  }
-  function mkResizer(fw, fh) {
-    return function tmp() {
-      pixelWidth = ($(window).width() - 15) * fw; // Hack to eliminate vertical scroll width
-      pixelHeight = $(window).height() * fh;
-      updateGeometry();
-    };
-  }
-  function mkCellReizer(s) {
-    return function tmp() {
-      pixelStep = s;
-      updateGeometry();
-    };
-  }
-  function fill(m) {
-    stop();
-    fillGrid(m);
-  }
-  $('#ctl_start').click(() => {
-    hold = false;
-    if (timerId !== undefined) {
-      clearTimeout(timerId);
-    }
-    go();
+  const g = new Canvas(new LGame());
+  g.height = $(window).height() * 0.7;
+  g.width = $(window).width() - 15; // scroll
+  g.cellStep = 2;
+  g.update();
+  g.fill(lifeMap);
+  const r = new Runner(g);
+
+  setInterval(() => {
+    $('#stat').text('FPS: ' + r.statFrames + ' T: ' + (r.statCalcTime / r.statFrames) + ' / ' + (r.statDrawTime / r.statFrames) + 'ms');
+    r.statFrames = 0;
+    r.statUpdated = 0; // TODO show updated
+    r.statCalcTime = 0;
+    r.statDrawTime = 0;
+  }, 1000);
+
+  $('#grid').on({
+    mousedown: (e) => {
+      r.stop();
+      g.touch(e.pageX, e.pageY, false);
+    },
+    mouseup: () => {
+      g.touchReset();
+    },
+    mousemove: (e) => {
+      if (e.buttons > 0) {
+        r.stop();
+        g.touch(e.pageX, e.pageY, true);
+      }
+    },
   });
-  $('#ctl_stop').click(stop);
-  $('#ctl_step').click(stepGrid);
-  $('#ctl_p_clear').click(() => { fill(() => {}); });
 
-  $('#ctl_fld_small').click(mkResizer(1, 0.7));
-  $('#ctl_fld_full_screen').click(mkResizer(1, 1));
-  $('#ctl_fld_double_screen').click(mkResizer(2, 2));
+  $('#ctl_start').click(() => { r.run(); });
+  $('#ctl_stop').click(() => { r.stop(); });
+  $('#ctl_step').click(() => { r.stop(); g.step(); });
+  $('#ctl_clear').click(() => { r.stop(); g.reset(); });
 
-  $('#ctl_p_life').click(() => { fill(lifeMap); });
+  $('[data-size-x]').each((n, v) => {
+    const e = $(v);
+    const fx = parseFloat(e.data('size-x'));
+    const fy = parseFloat(e.data('size-y'));
+    e.click(() => {
+      r.stop();
+      g.height = $(window).height() * fy;
+      g.width = $(window).width() * fx - 15; // scroll
+      g.update();
+    });
+  });
+  $('[data-cell-size]').each((n, v) => {
+    const e = $(v);
+    const s = parseInt(e.data('cell-size'), 10);
+    e.click(() => {
+      r.stop();
+      g.cellStep = s;
+      g.update();
+    });
+  });
+  $('#ctl_p_life').click(() => { r.stop(); g.fill(lifeMap); });
   const ee = [];
-  for (let i = 0; i < fillFunctions.length; i++) {
+  for (let i = 0; i < fillFunctions.length; ++i) {
     const p = fillFunctions[i];
-    const e = $('<button>').text(p[0]).click(() => { fill(p[1]); });
+    const e = $('<button>').text(p[0]).click(() => {
+      r.stop();
+      g.fill(p[1]);
+    });
     ee.push(' ', e);
   }
   $('#ctl_p_life').after(ee);
-
-  $('#ctl_cell_1').click(mkCellReizer(1));
-  $('#ctl_cell_2').click(mkCellReizer(2));
-  $('#ctl_cell_3').click(mkCellReizer(3));
-  $('#ctl_cell_5').click(mkCellReizer(5));
-  $('#ctl_cell_7').click(mkCellReizer(7));
-  $('#ctl_cell_10').click(mkCellReizer(10));
-  $('#ctl_cell_15').click(mkCellReizer(15));
-
-  $('#grid').mousemove((e) => {
-    if (hold) {
-      manualSetOnGrid(Math.floor((e.pageX - 1) / pixelStep), Math.floor((e.pageY - 1) / pixelStep), 1);
-    }
-  }).mousedown(() => { hold = true; })
-    .mouseup(() => { hold = false; })
-    .mouseout(() => { hold = false; })
-    .mouseleave(() => { hold = false; })
-    .click((e) => {
-      manualSetOnGrid(Math.floor((e.pageX - 1) / pixelStep), Math.floor((e.pageY - 1) / pixelStep));
-    });
-
-  pixelStep = 2;
-  mkResizer(1, 0.7)();
-  fillGrid(lifeMap);
-  timerId = setTimeout(go, 1200);
 });
